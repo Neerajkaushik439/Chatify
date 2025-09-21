a24d901fce7edc31168ca872cbcde063ec7a8672
Neeraj <neerajkaushik439@gmail.com>
2025-09-22 00:12:17 +0530

    fix: update CORS configuration to use the correct production URL

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors:{
-        origin: ['http://localhost:5173', 'https://chat-app-neon-theta.vercel.app'],
+        origin: ['http://localhost:5173', 'https://chatify-psi-hazel.vercel.app/'],
     }
 })
 
