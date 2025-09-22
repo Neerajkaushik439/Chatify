ec5e7d1db8e7d04ddea483a01f6506a590aa9085
Neeraj <neerajkaushik439@gmail.com>
2025-09-22 13:51:49 +0530

    changed the depencency

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,7 +2,7 @@ import cloudinary from "../lib/cloudniary.js";
 import generateToken from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
-import {generateState,generateCodeVerifier} from "arctic"
+
 
 
 
